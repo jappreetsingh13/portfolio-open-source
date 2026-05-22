c0e6cb0586744375522f373d439f6e52151197f5
jappreetsingh13 <jappreetsingh13.connect@gmail.com>
2026-05-22 23:44:58 +0530

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
       <About />
       <Skills />
       <Experience />
-      {/* <Projects /> */}
       <Stats />
       <Contact />
       <Footer />
