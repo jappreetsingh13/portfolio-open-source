431bfe1806d614b38834851d95c68d595f77d0ae
jappreetsingh13 <jappreetsingh13.connect@gmail.com>
2026-05-22 23:51:27 +0530

    Vercel Analytics & Insights with Resolved Build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,15 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Jappreet Singh | Full Stack Developer",
   description:
-    "Portfolio of Jappreet Singh — Next.js, React, AEM & Full Stack Developer",
+    "Portfolio of Jappreet Singh — Full Stack (React, Nextm MongoDB, Express) and AEM Developer(Sites)",
 };
 export default function RootLayout({
   children,
@@ -17,7 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`antialiased`}>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <>
+          {/* Vercel Analytics */}
+          <Analytics />
+
+          {/* Vercel Speed Insights */}
+          <SpeedInsights />
+          {children}
+        </>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "@/components/Hero";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
-import Projects from "@/components/Projects";
 import Stats from "@/components/Stats";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
